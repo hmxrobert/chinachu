9649a782249d9262389d44138868d9c3dc9500aa
Tsuruno Sato <hmx_robert@hotmail.com>
2017-01-10 09:40:01 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+if [ -e /var/run/chinachu-operator.pid ]; then
+  rm /var/run/chinachu-operator.pid
+fi
+
+if [ -e /var/run/chinachu-wui.pid ]; then
+  rm /var/run/chinachu-wui.pid
+fi
+
 /etc/init.d/chinachu-operator start
 /etc/init.d/chinachu-wui start
 
